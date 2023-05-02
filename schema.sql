80ecafc66384abb664182b2eb9b14fc795717194
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-02 20:04:01 +0430

    Create the animals table

---
+++ schema.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE animals (
+    id int GENERATED ALWAYS AS IDENTITY, 
+    name varchar(30), 
+    date_of_birth date, 
+    escape_attempt int, 
+    neutered boolean, 
+    weight_kg float
+);
