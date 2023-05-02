7934f71ebd13d0b489fecb4b9db7591b41debd6e
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-02 19:57:57 +0430

    Remove the unnecessary spaces

--- queries.sql
+++ queries.sql
@@ -1,5 +1,4 @@
 
-
 select * from animals where name like '%mon%';
 select name from animals where date_of_birth between '2016-01-01' and '2019-12-30';
 select name from animals where neutered = true and escape_attempt < 3;
