b6a2af0ee818dd355bde67a351bba783078dfed0
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-02 22:05:08 +0430

    Used Transactions

--- queries.sql
+++ queries.sql
@@ -1,4 +1,6 @@
 
+-- First Project
+
 select * from animals where name like '%mon%';
 select name from animals where date_of_birth between '2016-01-01' and '2019-12-30';
 select name from animals where neutered = true and escape_attempt < 3;
@@ -7,3 +9,48 @@ select name, escape_attempt from animals where weight_kg > 10.5;
 select * from animals where neutered = 'true';
 select * from animals where name != 'Gabumon';
 select * from animals where weight_kg between 10.3 and 17.4;
+
+
+-- Second Project
+
+begin;
+update animals set species = 'unspecified';
+select * from animals;
+
+rollback;
+select * from animals;
+
+UPDATE animals 
+SET species = 'digimon'
+WHERE name like '%mon%' ;
+select * from animals;
+
+UPDATE animals 
+SET species = 'pokemon'
+WHERE species is null;
+select * from animals;
+
+commit;
+
+-- Transactions --
+
+begin;
+delete from animals;
+select * from animals;
+
+rollback;
+select * from animals;
+
+delete from animals 
+where date_of_birth > '2022-01-01';
+
+savepoint delete2022;
+
+update animals set weight_kg = weight_kg * (-1);
+
+rollback to delete2022;
+
+update animals set weight_kg = weight_kg * (-1) where weight_kg < 0;
+select * from animals;
+commit ;
+
