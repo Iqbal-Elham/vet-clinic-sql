e4166ec3a5a154dc513d67a1c17d65048f18a190
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-03 22:47:06 +0430

    Create the owner and species table and linked to animals table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,12 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+CREATE TABLE owners (id int primary key generated always as identity, full_name varchar(50), age int);
+CREATE TABLE species (id int primary key generated always as identity,name varchar(50));
+
+ALTER TABLE animals DROP column species;
+ALTER TABLE animals ADD column species_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD column owner_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
