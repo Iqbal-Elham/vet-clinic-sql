0e518c336d6fb434aca37213d78e2f3bebb27222
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-05 20:22:32 +0430

    Add visits table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,4 @@ ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES ow
 
 CREATE TABLE vets (id int primary key generated always as identity, name varchar(50), age int, date_of_graduation date);
 CREATE TABLE specializations ( species_id int, vets_id int, primary key(species_id, vets_id), CONSTRAINT fk_species FOREIGN key(species_id) REFERENCES species(id), CONSTRAINT fk_vets FOREIGN key(vets_id) REFERENCES vets(id));
+CREATE TABLE visits ( animals_id int, vets_id int, date_of_visit date,  CONSTRAINT fk_animals FOREIGN key(animals_id) REFERENCES animals(id), CONSTRAINT fk_vets FOREIGN key(vets_id) REFERENCES vets(id));
